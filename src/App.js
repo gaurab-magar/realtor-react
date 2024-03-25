aea67848d48aa41fd30675f3ca00e5d2ef7154fb
HP <gdikshan10@gmail.com>
2024-03-26 01:27:57 +0545

    first environment build and template initialzed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className='bg-slate-500 text-sky-300'>hi my  name is gaurab thapa magar</h3>
+
     </div>
   );
 }
