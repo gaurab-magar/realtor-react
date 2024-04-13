0341ea7a1b57ded7b8c613cc150b93a3db2caacd
HP <gdikshan10@gmail.com>
2024-04-14 00:42:12 +0545

    home page decorated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,14 +30,14 @@ export const Header = () => {
           <img className='h-16 cursor-pointer' src={navbarImg} alt='navbar-img'></img>
         </Link>
         <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link to='/'>
-            <a className={`mr-5 cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Home</a>
+          <Link to='/'
+             className={`mr-5 cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Home
           </Link>
-          <Link to='/offers'>
-            <a className={`mr-5 cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/offers') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Offers</a>
+          <Link to='/offers'
+           className={`mr-5 cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/offers') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Offers
           </Link>
-          <Link to='/Contact'>
-            <a className={`mr-5 cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/Contact') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Contact Us</a>
+          <Link to='/Contact'
+             className={`mr-5 cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/Contact') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Contact Us
           </Link>
         </nav>
         {pageState ? 
@@ -52,33 +52,3 @@ export const Header = () => {
 
   )
 }
-
-
-
-{/* <header>
-        <div className='bg-black px-10 shadow-sm sticky top-0 z-50 flex justify-between items-center'>
-          <div>
-            <Link to="/">
-              <img className='h-16 cursor-pointer' src={navbarImg} alt='navbar-img'></img>
-            </Link>
-          </div>
-          <div>
-            <ul className='flex space-x-10 color-white text-base font-thin'>
-              <Link to='/'>
-                <li className={`cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Home</li>
-              </Link>
-              <Link to='/offers'>
-                <li className={`cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/offers') && "text-light border-b-yellow-400 border-b-[3px]"}`}>Offers</li>
-              </Link>
-              {pageState ? 
-                <Link to='/profile'>
-                  <li className={`cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/profile') && "text-light border-b-yellow-400 border-b-[3px]"}`}>{pageState}</li>
-                </Link>:
-                <Link to='/signin'>
-                  <li className={`cursor-pointer py-5 px-2 hover:text-yellow-500 text-gray-400 ${pathMatchRoute('/signin') && "text-light border-b-yellow-400 border-b-[3px]"}`}>{pageState}</li>
-                </Link>
-              }
-            </ul>
-          </div>
-        </div>
-    </header> */}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'swiper/css';
 import { Slider } from '../Components/Slider';
 import { ListingItem } from '../Components/ListingItem';
+import { FaCheck } from "react-icons/fa6";
 
 export const Home = () => {
   let listingRent = [
@@ -49,6 +50,27 @@ export const Home = () => {
         </div>
       </section>
       <section className='py-7'>
+        <div className=' flex flex-col justify-center items-center space-y-5'>
+            <h1 className='font-thin text-center text-3xl text-white-900'>The Goals</h1>
+            <p className='font-serif text-center text-md text-white w-full p-1 md:w-2/3'>
+            Landmark Sotheby’s International Realty wanted to elevate their agent recruitment and retention, build brand recognition for their business, empower their agents to build their personal brands and engage more leads, and stay cost efficient while doing so.
+            </p>
+        </div>
+        <div className='mx-1 md:mx-8 my-6 p-1 md:flex overflow-hidden gap-6 '>
+          <div className='w-full md:w-2/3'>
+              <img className='object-center object-cover h-96 w-full rounded-md' alt='homeImg1' src='https://www.luxurypresence.com/wp-content/uploads/2023/08/big-left-1-min-4-1200x720.jpg'></img>
+          </div>
+          <div className='hidden md:block md:w-1/3 space-y-3 mt-2 md:mt-0'>
+            <div className='h-1/2'>
+              <img className='object-center object-cover h-44' alt='homeImg1' src='https://www.luxurypresence.com/wp-content/uploads/2023/08/small-up-1-min-4-1200x720.jpg'></img>
+            </div>
+            <div className='w-full md:h-1/2'>
+              <img className='object-center object-cover h-44' alt='homeImg1' src='https://www.luxurypresence.com/wp-content/uploads/2023/08/small-down-1-min-4-1200x720.jpg'></img>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='py-7'>
         <div>
           <h1 className="font-bold text-center text-2xl text-white-600">Rent listing</h1>
           <p className="w-full text-center">Find your dream apartment here</p>
@@ -60,6 +82,41 @@ export const Home = () => {
             ))}
           </div>
         </div>
+      </section>
+      <section className='py-7 '>
+          <div className='container mx-auto'>
+            <div className='md:flex gap-3 p-1'>
+              <div className='w-full md:w-1/2'>
+                <h1 className='font-thin text-3xl text-white-900 mb-4'>The Solutions</h1>
+                <p>With agent subdomain sites, agents on the Landmark Sotheby’s International Realty team are able to easily set up their own beautiful, personalized websites. Benefits of using the product include:</p>
+                <div>
+                  <ul className='my-4'>
+                    <li className='text-gray-400 flex items-start gap-3'>
+                      <FaCheck className='text-yellow-400 text-xl' />
+                      <p className='text-sm'>
+                        <strong className='text-gray-200'>Fostering a Supportive Environment:</strong> Search by location or property type to find the perfect place for you.
+                      </p>
+                    </li>
+                    <li className='text-gray-400 flex items-start gap-3'>
+                      <FaCheck className='text-yellow-400 text-xl' />
+                      <p className='text-sm'>
+                        <strong className='text-gray-200'>Recruiting and Retaining Talent:</strong> having the strong feeling of home the perfect place for you.
+                      </p>
+                    </li>
+                    <li className='text-gray-400 flex items-start gap-3'>
+                      <FaCheck className='text-yellow-400 text-xl' />
+                      <p className='text-sm'>
+                        <strong className='text-gray-200'>Engaging More Leads:</strong> dont need yoga after sunbath find the perfect place for you.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className='w-full md:w-1/2 p-1'>
+                <img className='object-center object-cover rounded-md' alt='homeImg1' src='https://www.luxurypresence.com/wp-content/uploads/2023/08/big-right-1-min-4.jpg'></img>
+              </div>
+            </div>
+          </div>
       </section>
       <section className='py-7'>
         <div>
@@ -74,8 +131,13 @@ export const Home = () => {
           </div>
         </div>
       </section>
-
-      
+      <section className='py-7'>
+        <div className='container mx-auto flex items-center justify-center'>
+          <p className='w-full p-1 md:w-2/3 text-center font-serif text-4xl'>
+            Trusted by over 20 of the top 100 WSJ agents in the US
+          </p>
+        </div>
+        </section> 
     </div> 
   </main>
 );
