812439a77ad58bca9027b6ce86fdd3e380d08dc4
HP <gdikshan10@gmail.com>
2024-04-15 23:50:42 +0545

    fix bugs and succcessfully deployed

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -17,7 +17,7 @@ export const Contact = () => {
             <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
             <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
           </div>
-          <button className="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+          <button className="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Send</button>
           <p className="text-xs text-gray-500 mt-3">Send us an Email. </p>
         </div>
       </div>
